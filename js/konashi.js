756e25f4ed145a1cd21612fa78e5ac7d648d5c2e
Yusaku Nishiwaki <nishiwaki@ux-xu.com>
2020-03-06 10:53:07 +0900

    fix namePrefix 'konashi2' -> 'konashi'

--- js/konashi.js
+++ js/konashi.js
@@ -140,14 +140,14 @@ class Konashi {
    * Find konasih2 device
    *
    * @param {Boolean} [autoConnect]
-   * @param {Object} [options] defaul: `{filters: [{namePrefix: 'konashi2'}], optionalServices: ['229bff00-03fb-40da-98a7-b0def65c2d4b']}`
+   * @param {Object} [options] defaul: `{filters: [{namePrefix: 'konashi'}], optionalServices: ['229bff00-03fb-40da-98a7-b0def65c2d4b']}`
    * @returns {Promise<Konashi>}
    */
   static find(autoConnect, options) {
     if (typeof autoConnect == undefined) {
       autoConnect = true;
     }
-    options = options || {filters: [{namePrefix: 'konashi2'}], optionalServices: [Konashi._serviceUUID]};
+    options = options || {filters: [{namePrefix: 'konashi'}], optionalServices: [Konashi._serviceUUID]};
     return new Promise((resolve, reject) => {
       navigator.bluetooth
         .requestDevice(options)
